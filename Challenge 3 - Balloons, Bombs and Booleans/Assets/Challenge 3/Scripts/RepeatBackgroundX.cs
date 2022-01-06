e19ae43c01f5ca52331dcf30e7b9cc702e03e572
Atacan Kilic <uaatacan@gmail.com>
2022-01-06 19:58:00 +0300

    Added Challenge 3

---
+++ Challenge 3 - Balloons, Bombs and Booleans/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+    private PlayerControllerX playerControllerXScript;
+
+    private void Start()
+    {
+        playerControllerXScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+        startPos = transform.position; // Establish the default starting position 
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2; // Set repeat width to half of the background
+    }
+
+    private void Update()
+    {
+        // If background moves left by its repeat width, move it back to start position
+        if (transform.position.x < startPos.x - repeatWidth && !playerControllerXScript.gameOver)
+        {
+            transform.position = startPos;
+        }
+    }
+}
+
+
